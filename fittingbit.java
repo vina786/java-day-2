59461dab38ef5933e418525ff7d46d2406880ac3
vina786 <vinay76b@gmail.com>
2024-12-03 16:04:53 +0530

    Add files via upload

---
+++ fittingbit.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class fittingbit {
+    public static void main(String args[])
+    {
+        int a=12;
+        int b=1;
+        System.out.println(a^(1<<b));
+
+    }
+}
