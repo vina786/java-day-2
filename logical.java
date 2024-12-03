c4224140e6bfe4cc94227c105f56b2c24bb35b4e
vina786 <vinay76b@gmail.com>
2024-12-03 14:46:08 +0530

    Add files via upload

---
+++ logical.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class logical {
+    public static void main(String args[])
+    {
+        System.out.println(true&&true);
+        System.out.println(false||true);
+        System.out.println(!(true));
+        
+    }
+}
