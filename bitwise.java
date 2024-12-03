2edd8943168db5f0ede181fc95388d70661f3bb0
vina786 <vinay76b@gmail.com>
2024-12-03 15:16:20 +0530

    Add files via upload

---
+++ bitwise.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class bitwise {
+    public static void main(String args[])
+    {
+        int a=12,b=15;
+        System.out.println(a&b);
+        System.out.println(a|b);
+        System.out.println(a^b);
+    }
+}
