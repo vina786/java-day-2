5cbbd73e128c73f17e1cadc751f20ed7e32bea46
vina786 <vinay76b@gmail.com>
2024-12-03 10:15:38 +0530

    Add files via upload

---
+++ arithematic.java
@@ -0,0 +1,18 @@
+import java.util.*;
+public class arithematic{
+    public static void main(String args[]){
+        int a=5,b=10,c,d,e,f,h;
+        c=a+b;
+        d=a-b;
+        e=a*b;
+        f=a/b;
+        h=a%b;
+        System.out.println("a+b="+c);
+        System.out.println("a-b="+d);
+        System.out.println("a*b="+e);
+        System.out.println("a/b="+h);
+        System.out.println("a%b="+h);
+
+
+    }
+}
