9f5254ccb80966613155df16d919dbab097e4236
vina786 <vinay76b@gmail.com>
2024-12-03 10:58:53 +0530

    Add files via upload

---
+++ assignment.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class assignment {
+    public static void main(String[] args)
+    {
+       int a=45;
+        System.out.println(a+=46);
+        System.out.println(a-=9);
+        System.out.println(a*=2);
+     
+}
+}
