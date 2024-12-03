7b05e289ed8fd74cb4cd8944c02ddeac2c3b49bd
vina786 <vinay76b@gmail.com>
2024-12-03 10:24:13 +0530

    Add files via upload

---
+++ comparision.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class comparision {
+    public static void main(String[] args){
+        int a=45;
+        int b=54;
+        System.out.println(a==b);
+        System.out.println(a!=b);
+        System.out.println(a>=b);
+        System.out.println(a<=b);
+        System.out.println(a>b);
+        System.out.println(a<b);
+    }
+    
+}
